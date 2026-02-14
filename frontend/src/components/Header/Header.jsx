5fcd6371d6243271c92f06c77781e1787a825c47
JanithHisara <hisarajanith@gmail.com>
2026-02-14 14:20:25 +0530

    feat: add new `Header` component with basic layout and content

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,12 +4,12 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-        <div className="header-contents">
-            <h2>Order your favourite food hare</h2>
-            <p>Technology refers to the application of scientific knowledge, tools, techniques, and systems to solve problems, improve efficiency, and enhance the quality of life. It encompasses a wide range of innovations, from basic tools like the wheel to advanced systems like artificial intelligence, robotics, and communication networks. Technology plays a central role in shaping industries, communication, healthcare, education, and daily human activities.</p>
-            <button>View Menu</button>
-        </div>
-      
+      <div className="header-contents">
+        <h2>Janith</h2>
+        <p>Technology refers to the application of scientific knowledge, tools, techniques, and systems to solve problems, improve efficiency, and enhance the quality of life. It encompasses a wide range of innovations, from basic tools like the wheel to advanced systems like artificial intelligence, robotics, and communication networks. Technology plays a central role in shaping industries, communication, healthcare, education, and daily human activities.</p>
+        <button>View Menu</button>
+      </div>
+
     </div>
   )
 }
